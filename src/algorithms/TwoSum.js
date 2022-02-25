eef8e44c19075d028e41549908e61a43149e14f1
crayOmn <otmancr0@gmail.com>
2022-02-26 00:26:34 +0100

    Update TwoSum.js

--- src/algorithms/TwoSum.js
+++ src/algorithms/TwoSum.js
@@ -1,45 +1,15 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
  */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var addTwoNumbers = function (l1, l2) {
-  if (l1 === null || l2 === null) return null;
-
-  let dummyHead = new ListNode(0);
-  let cur1 = l1;
-  let cur2 = l2;
-  let cur = dummyHead;
-  let carry = 0;
-
-  while (cur1 !== null || cur2 !== null) {
-    let val1 = cur1 !== null ? cur1.val : 0;
-    let val2 = cur2 !== null ? cur2.val : 0;
-    let sum = val1 + val2 + carry;
-    let newNode = new ListNode(sum % 10);
-    carry = sum >= 10 ? 1 : 0; 
-    cur.next = newNode;
-    cur = cur.next;
-
-    if (cur1 !== null) {
-      cur1 = cur1.next;
-    }
-
-    if (cur2 !== null) {
-      cur2 = cur2.next;
-    }
-  }
-
-  if (carry > 0) {
-    cur.next = new ListNode(carry);
-  }
-
-  return dummyHead.next;
+var twoSum = function(nums, target) {
+    const map = new Map();
+      for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i];
+        if (map.has(diff)) {
+          return [map.get(diff), i];
+        }
+        map.set(nums[i], i);
+      }
 };
